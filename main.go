3e3a859c0e7ae75862adbba3292d0adefdbbc393
Robeel Bhatti <robeelbhatti19@gmail.com>
2026-06-14 23:24:38 -0400

    feat: add channel to handle OS signals and start reading byte stream.

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 func main() {
@@ -13,16 +18,24 @@ func main() {
 	}
 	defer l.Close()
 
+	sigCh := make(chan os.Signal, 1) // buffered channel otherwise sender will block and discard signal
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// spawn a separate goroutine that blocks and listens to channel
+	go func() {
+		sig := <-sigCh
+		log.Printf("received signal: %v. shutting down...", sig)
+		l.Close()
+	}()
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			if errors.Is(err, net.ErrClosed) {
-				log.Fatalf("listener has shutdown")
-				// end program execution
+				log.Println("listener shut down")
+				break
 			}
 			log.Printf("failed to accept: %v", err)
-
-			// skip processing this connection and move to the next one
 			continue
 		}
 		go handleConn(conn)
@@ -30,5 +43,27 @@ func main() {
 }
 
 func handleConn(conn net.Conn) {
+	r := bufio.NewReader(conn)
+	for {
+		rline, err := r.ReadString('\n')
+		if err != nil {
+			log.Printf("failed to read: %v", err)
+			break
+		}
+		rline = strings.TrimRight(rline, "\r\n")
+		log.Print(rline)
 
+		for {
+			hline, err := r.ReadString('\n')
+			if err != nil {
+
+			}
+
+			hline = strings.TrimRight(hline, "\r\n")
+			if hline == "" {
+				return
+			}
+			log.Print(hline)
+		}
+	}
 }
